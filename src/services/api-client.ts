b66000559e1cd9e9ccff343b7c70617eed05b295
jamshale <jamiehalebc@gmail.com>
2023-04-23 16:07:10 -0700

    Refactor - React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "2108f5fdcffe4ff8ae89b5ff89837ad2",
   },
 });
